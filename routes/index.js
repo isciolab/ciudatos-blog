9a2918d5baf2c588cf300344013843a1876ae8a6
David Daza <03dazal@gmail.com>
2017-11-02 16:23:16 -0500

    Partial profile city page

--- routes/index.js
+++ routes/index.js
@@ -42,6 +42,7 @@ exports = module.exports = function (app) {
 	app.get('/datos', routes.views.datasets);
 	app.get('/informes', routes.views.documents);
 	app.get('/blog', routes.views.blog);
+	app.get('/ciudades', routes.views.city);
 	app.get('/blog/:category?', routes.views.blog);
 	app.get('/blog/post/:post', routes.views.post);
 	app.get('/gallery', routes.views.gallery);
---
+++ routes/views/city.js
@@ -0,0 +1,7 @@
+const keystone = require('keystone');
+
+exports = module.exports = function(req, res) {
+  const view = new keystone.View(req, res);
+  let locals = res.locals;
+  view.render('city');
+}
