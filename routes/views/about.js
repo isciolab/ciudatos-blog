4127b1d627ba6022247fb7f70cd198f0a992fa49
David Daza <03dazal@gmail.com>
2017-11-30 12:18:13 -0500

    Minor changes

--- routes/views/about.js
+++ routes/views/about.js
@@ -3,5 +3,5 @@ const keystone = require('keystone');
 exports = module.exports = function(req, res) {
   const view = new keystone.View(req, res);
   const locals = res.locals;
-  view.render('city');
+  view.render('about');
 }
