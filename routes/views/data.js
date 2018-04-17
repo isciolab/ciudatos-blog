03ee32a0da582381941afd91c547e09e3b242d2c
David Daza <03dazal@gmail.com>
2018-04-17 11:41:07 -0500

    Routing to state inside iframe app

--- routes/views/data.js
+++ routes/views/data.js
@@ -3,6 +3,9 @@ const keystone = require('keystone');
 exports = module.exports = function (req, res) {
 	const view = new keystone.View(req, res);
 	const locals = res.locals;
+	const section = req.query.data || undefined
+	locals.data = {}
+	locals.data.iframe = section ? `https://randommonkey.shinyapps.io/appDownload/?data=${section}` : `https://randommonkey.shinyapps.io/appDownload/`
 	locals.title = 'Sobre Nosotros | Ciudatos';
 	locals.section = 'base-de-datos';
 	view.render('bases-de-datos');
