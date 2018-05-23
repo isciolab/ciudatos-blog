6a450c36f15e7fa9db3470b462c97517d5eff650
David Daza <03dazal@gmail.com>
2018-05-23 18:07:08 -0500

    Added file cards

--- routes/views/city.js
+++ routes/views/city.js
@@ -4,14 +4,20 @@ exports = module.exports = function (req, res, next) {
 	const view = new keystone.View(req, res);
 	const locals = res.locals;
 	const { city } = req.params;
-	const cities = ['barranquilla', 'bogota', 'bucaramanga', 'cali', 'cali',  'cartagena', 'cucuta', 'ibague', 'manizales', 'medellin', 'pereira', 'santa-marta'];
+	// const cities = ['barranquilla', 'bogota', 'bucaramanga', 'cali', 'cali',  'cartagena', 'cucuta', 'ibague', 'manizales', 'medellin', 'pereira', 'santa-marta'];
+	const cities = ['cali',  'cartagena', 'manizales', 'medellin', 'santa-marta'];
+
+	function capitalize (str) {
+		if (!str) return
+		return str.charAt(0).toUpperCase() + str.substr(1).toLowerCase()
+	}
 	
 	locals.data = {
-		city: city || 'bogota',
+		city: city || 'cali',
 	};
 
 	if (cities.indexOf(locals.data.city) > -1) {
-		locals.title = `Ciudades - | Ciudatos`;
+		locals.title = `Ciudades - ${capitalize(locals.data.city)} | Ciudatos`;
 		locals.section = 'ciudades';
 		view.render('city');
 	} else {
