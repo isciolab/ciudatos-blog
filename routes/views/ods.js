63e984f91e6442d0813a317a174a43d688f5132c
Jhainey <milevisj@gmail.com>
2019-01-30 11:33:05 -0500

    sdsad

--- routes/views/ods.js
+++ routes/views/ods.js
@@ -8,6 +8,6 @@ exports = module.exports = function (req, res) {
 	const { section } = req.query;
 	locals.data = {};
 	locals.data.section = section;
-	//view.render('ods');
-	view.render('index');
+	view.render('ods');
+	
 };
--- routes/views/visualize.js
+++ routes/views/visualize.js
@@ -8,6 +8,6 @@ exports = module.exports = function (req, res) {
 	const { section } = req.query;
 	locals.data = {};
 	locals.data.section = section;
-	//view.render('visualize');
-	view.render('index');
+	view.render('visualize');
+
 };
