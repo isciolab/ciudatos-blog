4866852ec104aca9223053fbecae419b72907b35
Jhainey <milevisj@gmail.com>
2019-03-30 19:02:14 -0500

    dsadas

--- routes/views/data.js
+++ routes/views/data.js
@@ -5,7 +5,7 @@ exports = module.exports = function (req, res) {
 	const locals = res.locals;
 	const section = req.query.data || undefined
 	locals.data = {}
-	locals.data.iframe = section ? `http://88.208.3.175:3838/ciudatos-app-2/appDownload/?data=${section}` : `http:/88.208.3.175:3838/ciudatos-app-2/appDownload/`
+	locals.data.iframe = section ? `http://169.61.55.8:3838/ciudatos-app-2/appDownload/?data=${section}` : `http://169.61.55.8:3838/ciudatos-app-2/appDownload/`
 	locals.title = 'Bases de datos | Ciudatos';
 	locals.section = 'base-de-datos';
 	view.render('bases-de-datos');
